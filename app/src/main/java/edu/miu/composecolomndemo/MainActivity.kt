1bcc936f558f45a0bf44088466e083fa18695c30
Bright Gee Varghese <brightlsom@gmail.com>
2024-07-06 15:37:34 -0500

    initial commit

--- app/src/main/java/edu/miu/composecolomndemo/MainActivity.kt
+++ app/src/main/java/edu/miu/composecolomndemo/MainActivity.kt
@@ -5,18 +5,26 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import edu.miu.composecolomndemo.ui.theme.ComposeColomnDemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,23 +34,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeColomnDemoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Column(
-                        modifier = Modifier.fillMaxSize(),
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        verticalArrangement = Arrangement.Top
-                    ) {
-                        MyImage(
-                            imageId = R.drawable.miu_logo,
-                            imageDescription = "Miu Logo",
-                            modifier = Modifier
-                                .padding(innerPadding)
-                        )
-                        MyText(
-                            text = "Maharishi International University",
-                            modifier = Modifier
-                                .padding(10.dp)
-                        )
-                    }
+                    MIUHome(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -50,47 +42,30 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyImage(imageId: Int, imageDescription: String, modifier: Modifier = Modifier) {
-    Image(
-        painter = painterResource(id = imageId),
-        contentDescription = imageDescription,
-        modifier = modifier
-    )
-}
-
-@Composable
-fun MyText(text: String, modifier: Modifier = Modifier) {
-    Text(
-        text = text,
-        modifier = modifier
-    )
+fun MIUHome(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.miu_logo),
+            contentDescription = "Miu Logo",
+            modifier = Modifier
+                .border(width = 2.dp, color = Color(0xFF28CE7D), shape = CircleShape)
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = "Maharishi International University",
+            fontSize = 20.sp,
+        )
+    }
 }
 
 @Composable
 @Preview(showBackground = true)
 fun DefaultPreview() {
-    Column(
-        modifier = Modifier
-            .fillMaxSize(),
-        verticalArrangement = Arrangement.Top,
-//        verticalArrangement = Arrangement.Bottom,
-//        verticalArrangement = Arrangement.Center,
-//        verticalArrangement = Arrangement.SpaceEvenly,
-//        verticalArrangement = Arrangement.SpaceBetween,
-//        verticalArrangement = Arrangement.SpaceAround
-        horizontalAlignment = Alignment.CenterHorizontally
-//        horizontalAlignment = Alignment.Start
-//        horizontalAlignment = Alignment.End
-    ) {
-        MyImage(
-            imageId = R.drawable.miu_logo,
-            imageDescription = "Miu Logo",
-            modifier = Modifier
-                .padding(top = 20.dp, bottom = 20.dp)
-        )
-        MyText(
-            text = "Maharishi International University",
-            modifier = Modifier
-        )
+    ComposeColomnDemoTheme {
+        MIUHome()
     }
 }
